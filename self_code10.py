67f4c7e47a6783b68093e2d00535ece1195d2511
Shaurya Pratap Singh <rgshadow1001@gmail.com>
2026-04-14 14:08:38 +0000

    Level 10: Functions with return statementswith fun

---
+++ self_code10.py
@@ -0,0 +1,5 @@
+def multiply(a , b):
+    return a*b
+def call():
+    print(multiply(int(input("Enter first number: ")),int(input("Enter second number: "))))
+call()
--- self_code9.py
+++ self_code9.py
@@ -1,6 +1,9 @@
 total=0
 x=1
+def add(y):
+    global total
+    total+=y
 while x!=0:
     x=int(input("Enter a number: "))
-    total+=x
-print(total)
+    add(x)
+print(f"the total sum is {total}")
